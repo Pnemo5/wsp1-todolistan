ae625c184da7cce8d4760f9966a496c9187056a3
Pnemo5 <elin.ly.wallgren@gmail.com>
2025-11-27 16:54:05 +0100

    Lektion1
    Har lagt till alla todos, börjat med hur man lägger till en todo

--- app.rb
+++ app.rb
@@ -8,15 +8,40 @@ class App < Sinatra::Base
     def db
       return @db if @db
 
-      @db = SQLite3::Database.new(DB_PATH)
+      @db = SQLite3::Database.new("db/todos.sqlite.db")
       @db.results_as_hash = true
 
       return @db
     end
 
-    # Routen /
+    #Routen redirectar till /todos
     get '/' do
-        erb(:"index")
+      redirect('/todos')
+    end
+
+    # Routen hämtar alla todos i databasen
+    get '/todos' do
+      @todos = db.execute('SELECT * FROM todos')
+      p @todos
+      erb(:"todos/index")
+    end
+
+    #skickar en till ett formulär för att skapa en ny todo
+    get '/todos/new' do
+      erb(:"todos/new")
+    end
+
+    #Routen sparar todon och redirectar sedan till /todos
+    post '/todos' do
+      p params
+      t_name = params["todo_name"]
+      t_description = params["todo_description"]
+      t_category = params["todo_category"]
+      t_priority = params["todo_priority"]
+
+      db.execute("INSERT INTO todos (name, description, category, priority)
+      VALUES(?, ?, ?, ?)", [t_name, t_description, t_category, t_priority])
+      redirect("/todos")
     end
 
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -4,7 +4,7 @@ require_relative '../config'
 class Seeder
 
   def self.seed!
-    puts "Using db file: #{DB_PATH}"
+    puts "Using db file: #{'db/todos.sqlite.db'}"
     puts "🧹 Dropping old tables..."
     drop_tables
     puts "🧱 Creating tables..."
@@ -27,16 +27,16 @@ class Seeder
   end
 
   def self.populate_tables
-    db.execute('INSERT INTO todos (name, description) VALUES ("Köp mjölk", "3 lite mellanmjölk, eko")')
-    db.execute('INSERT INTO todos (name, description) VALUES ("Köp julgran", "En rödgran")')
-    db.execute('INSERT INTO todos (name, description) VALUES ("Pynta gran", "Glöm inte lamporna i granen och tomten")')
+    db.execute('INSERT INTO todos (name, description, category, priority) VALUES ("Köp mjölk", "3 lite mellanmjölk, eko", inköp, 1)')
+    db.execute('INSERT INTO todos (name, description, category, priority) VALUES ("Köp julgran", "En rödgran", inköp, 1)')
+    db.execute('INSERT INTO todos (name, description, category, priority) VALUES ("Pynta gran", "Glöm inte lamporna i granen och tomten", övrigt, 1)')
   end
 
   private
 
   def self.db
     @db ||= begin
-      db = SQLite3::Database.new(DB_PATH)
+      db = SQLite3::Database.new('db/todos.sqlite.db')
       db.results_as_hash = true
       db
     end
